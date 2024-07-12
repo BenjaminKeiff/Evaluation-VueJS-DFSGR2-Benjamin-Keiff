34c2c3a970a66b60099be11351ae8942d09ade39
Benjamin Keiff <benjamin.keiff@aperam.com>
2024-07-12 17:50:42 +0200

    fix trouble with client interface

--- frontend/src/interfaces/client.js
+++ frontend/src/interfaces/client.js
@@ -10,16 +10,8 @@ const clientInterface = {
   address2: '',
   zipcode: '',
   city: '',
-  country:'',
-  createdAt: this.item.createdAt = (() => {
-    const date = new Date(Date.now());
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-})()
-
+  country: '',
+  createdAt: Date.now()
 }
 
-export { clientInterface };
-
+export { clientInterface }
